2704101b6b1f2542e0af9ecb7f9646e0df897bc1
Becca Refford <beccarefford@gmail.com>
2016-01-23 16:30:22 -0500

    fixing read more on archive blog page

--- wp-content/themes/techgirlz/functions.php
+++ wp-content/themes/techgirlz/functions.php
@@ -172,8 +172,7 @@ require get_template_directory() . '/inc/jetpack.php';
 
 
 function wpse_custom_excerpts($content, $limit, $permalink) {
-    return wp_trim_words($content, $limit, '<a href="'. esc_url( $permalink ) .
-          '">' . '&nbsp;&hellip;' . __( 'Read more &nbsp;&raquo;', 'wpse' ) . '</a>');
+    return wp_trim_words($content, $limit, '&nbsp;&hellip;' );
 }
 
 

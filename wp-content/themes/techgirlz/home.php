64cedff0498b70eb7e8639223a84706882979e48
Becca Refford <beccarefford@gmail.com>
2016-01-30 18:14:12 -0500

    homepage cleanup

--- wp-content/themes/techgirlz/home.php
+++ wp-content/themes/techgirlz/home.php
@@ -9,7 +9,7 @@
       </div>
 
       <div class="col-md-6">
-        <i class="fa fa-arrow-circle-right fa-5x"></i>
+        <i class="fa fa-arrow-circle-right fa-5x" onmouseover="this.style.color='#F5F5F5';" onmouseout="this.style.color='#ffffff';"></i>
       </div>
     </div>
   </div>
@@ -58,17 +58,23 @@
   <a href="/blog/"><i class="fa fa-users fa-5x"></i></a>
   <h2>Why We Do It</h2>
     <p>We believe it takes a village to change girls' minds about tech,
-    so we're enabling communities to engage girls.</p>
+    so we're enabling communities to engage them.</p>
   </center>
   </div>
 </div>
 
 <div class="spacing">&nbsp;</div>
 <hr />
+<div class="spacing">&nbsp;</div>
+
+<center><h3>After just one of our workshops...</h3></center>
 
   <div class="row">
     <div class="col-md-6">
       <h1><span>81%</span></h1>
+      <p>
+        of girls were said they were more interested in technology.
+      </p>
       <span><i class="fa fa-female fa-5x"></i>
       <i class="fa fa-female fa-5x"></i>
       <i class="fa fa-female fa-5x"></i>
@@ -80,14 +86,13 @@
       </span>
       <i class="fa fa-female fa-5x"></i>
       <i class="fa fa-female fa-5x"></i>
-      <br />
-      <br /><p>
-        Of girls were more interested in technology after attending <a href="/techshopz">one of our workshops</a>.
-      </p>
     </div>
 
     <div class="col-md-6">
       <h1><span>70%</span></h1>
+      <p>
+        of girls said they were more likely to consider a career in technology.
+      </p>
         <span><span>
         <i class="fa fa-female fa-5x"></i>
         <i class="fa fa-female fa-5x"></i>
@@ -100,11 +105,6 @@
         <i class="fa fa-female fa-5x"></i>
         <i class="fa fa-female fa-5x"></i>
         <i class="fa fa-female fa-5x"></i>
-      <br />
-      <br />
-      <p>
-        Of girls said they were more likely to pursue a career in technology after <a href="/techshopz">one of our workshops.</a>
-      </p>
     </div>
   </div>
 

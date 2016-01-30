3bc19cbf16e3e532284323e8d82ee5e7e86ad7d8
Becca Refford <beccarefford@gmail.com>
2016-01-30 11:18:11 -0500

    adding new workshop categories to TIB page

--- wp-content/themes/techgirlz/tibs.php
+++ wp-content/themes/techgirlz/tibs.php
@@ -83,68 +83,47 @@ Template Name: TechShopz In A Box
 <div class="spacing">&nbsp;</div>
 
 <div class="row">
-<div class="col-md-6">
-  <center><h1>Topics</h1></center>
-  <p>
-    We have tons of workshop plans covering <a href="/techshopzinfo">a wide variety of topics</a>. Here
-    are a few of our most popular:
-  </p>
-  <center>
-
-  <a href="/techshop-topics">
-  <div class="anywhere-circle">
-    <img alt="HTML & CSS TechGirlz" src="/wp-content/uploads/2016/01/Screen-Shot-2016-01-15-at-1.35.09-PM.png">
+  <div class="col-md-12">
+    <center><h1>Workshop Topic Categories</h1>
   </div>
-  <h2>HTML & CSS</h2>
-
-  <div class="anywhere-circle">
-    <img alt="Blockly TechGirlz" src="/wp-content/uploads/2016/01/Screen-Shot-2016-01-15-at-1.42.30-PM.png">
-  </div>
-  <h2>Blockly</h2>
-
-  <div class="anywhere-circle">
-    <img alt="Mobile Apps TechGirlz" src="/wp-content/uploads/2016/01/Screen-Shot-2016-01-15-at-1.39.34-PM.png">
-  </div>
-  <h2>Designing Mobile Apps</h2>
-  </center>
-  </a>
 </div>
 
-<div class="col-md-6">
-    <center><h1>The 5 Steps</h1></center>
-    <div class="spacing">&nbsp;</div>
-          <h3>1. Sign Up</h3>
-            <p>
-            <a target="_blank" href="http://webforms.zenginehq.com/d783814e989d11d4fac077f7ec55894ddf91bf55d95b689784">Get free access to TechShopz in a Box™,</a> which includes over 20
-            individual workshop plans and the <a href="/run-your-own-techshopz-in-a-box-playbook/">TechGirlz PlayBook</a> with tips and
-            guidelines to organize your own workshop.
-            </p>
+<div class="spacing">&nbsp;</div>
 
-          <h3>2. Choose Topic</h3>
-            <p>
-              Review our <a href="/techshopzinfo/">workshop plan topics</a> and select one that
-              matches your expertise or interests.
-            </p>
-
-          <h3>3. Find Instructor</h3>
-            <p>
-              Not a tech expert? Not a problem! Read our tips on finding a
-              local instructor in the <a href="/run-your-own-techshopz-in-a-box-playbook/">TechGirlz PlayBook.</a>
-            </p>
-
-          <h3>4. Schedule Location</h3>
-            <p>
-              Use a conference room in your office or contact local libraries
-              and community centers to find space.
-            </p>
-
-          <h3>5. Schedule Location</h3>
-            <p>
-              Let us know when your workshop is scheduled so we can share
-              it on the TechGirlz website. Tell us at least 6 weeks before the
-              workshop, and we'll help you find girls to attend.
-            </p>
+<div class="row">
+  <div class="col-md-2">
+    <a href="/techshop-topics">
+    <div class="anywhere-circle">
+      <img alt="Web Development TechGirlz" src="/wp-content/uploads/2016/01/Screen-Shot-2016-01-15-at-1.35.09-PM.png">
+    </div>
+    <p>Web Development</p>
   </div>
+
+  <div class="col-md-2">
+    <div class="anywhere-circle">
+      <img alt="Game Design TechGirlz" src="/wp-content/uploads/2016/01/Screen-Shot-2016-01-30-at-11.08.39-AM.png">
+    </div>
+    <p>Game Design & Development</p>
+  </div>
+
+  <div class="col-md-2">
+    <div class="anywhere-circle">
+      <img alt="Mobile Apps TechGirlz" src="/wp-content/uploads/2016/01/Screen-Shot-2016-01-15-at-1.39.34-PM.png">
+    </div>
+    <p>Mobile Apps</p>
+  </div>
+
+  <div class="col-md-2">
+    <div class="anywhere-circle">
+      <img alt="Media Editing TechGirlz" src="/wp-content/uploads/2016/01/Screen-Shot-2016-01-30-at-11.11.38-AM.png">
+    </div>
+    <p>Media Editing</p>
+  </div>
+
+
+</div>
+</center>
+</a>
 </div>
 
 <div class="spacing">&nbsp;</div>

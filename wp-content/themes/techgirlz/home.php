79dbec107404af3884018ba4c6ce78ef8ded00af
Becca Refford <beccarefford@gmail.com>
2016-01-30 10:33:53 -0500

    home page fixed

--- wp-content/themes/techgirlz/home.php
+++ wp-content/themes/techgirlz/home.php
@@ -35,54 +35,41 @@
 
 <div id="home-content">
   <div class="row">
-    <div class="col-md-4">
-      <h2>What We Do</h2>
-        <p>TechGirlz empowers 13-15 year old girls to explore technology through
-          free, hands-on workshops.</p>
-    </div>
-
-    <div class="col-md-4">
-      <h2>How We Do It</h2>
-        <p>By creating open curricula of technical topics that are free to all.
-        This approach helps us reach more girls in more places.</p>
-    </div>
-
-    <div class="col-md-4">
-      <h2>Why We Do It</h2>
-        <p>We believe it takes a village to change girls' minds about tech,
-        so we're enabling communities to expose them to it.</p>
-    </div>
+  <div class="col-md-4">
+  <center>
+  <a href="/techshopz/"><i class="fa fa-cogs fa-5x"></i></a>
+  <h2>What We Do</h2>
+    <p>We empower middle school girls to explore technology through
+      free, hands-on workshops.</p>
+  </center>
   </div>
+
+  <div class="col-md-4">
+  <center>
+  <a href="/techshopz-in-a-box/"><i class="fa fa-download fa-5x"></i></a>
+  <h2>How We Do It</h2>
+    <p>By creating tech-centered workshop plans that are free to all.
+    This approach helps us reach more girls in more places.</p>
+  </center>
+  </div>
+
+  <div class="col-md-4">
+  <center>
+  <a href="/blog/"><i class="fa fa-users fa-5x"></i></a>
+  <h2>Why We Do It</h2>
+    <p>We believe it takes a village to change girls' minds about tech,
+    so we're enabling communities to engage girls.</p>
+  </center>
+  </div>
+</div>
 
 <div class="spacing">&nbsp;</div>
 <hr />
 
   <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-6">
       <h1><span>81%</span></h1>
-      <p>
-        Of girls were more interested in technology after attending one of our workshops.
-      </p>
-    </div>
-
-    <div class="col-md-8">
       <span><i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
       <i class="fa fa-female fa-5x"></i>
       <i class="fa fa-female fa-5x"></i>
       <i class="fa fa-female fa-5x"></i>
@@ -93,54 +80,30 @@
       </span>
       <i class="fa fa-female fa-5x"></i>
       <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="col-md-8">
-      <span><span><i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-    </span>
-    </span>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
+      <br />
+      <br /><p>
+        Of girls were more interested in technology after attending <a href="/techshopz">one of our workshops</a>.
+      </p>
     </div>
 
-
-    <div class="col-md-4">
+    <div class="col-md-6">
       <h1><span>70%</span></h1>
+        <span><span>
+        <i class="fa fa-female fa-5x"></i>
+        <i class="fa fa-female fa-5x"></i>
+        <i class="fa fa-female fa-5x"></i>
+        <i class="fa fa-female fa-5x"></i>
+        <i class="fa fa-female fa-5x"></i>
+        <i class="fa fa-female fa-5x"></i>
+        <i class="fa fa-female fa-5x"></i>
+      </span></span>
+        <i class="fa fa-female fa-5x"></i>
+        <i class="fa fa-female fa-5x"></i>
+        <i class="fa fa-female fa-5x"></i>
+      <br />
+      <br />
       <p>
-        Of girls said they were more likely to pursue a career in technology after one of our workshops.
+        Of girls said they were more likely to pursue a career in technology after <a href="/techshopz">one of our workshops.</a>
       </p>
     </div>
   </div>

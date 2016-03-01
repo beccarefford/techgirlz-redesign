bff42142834f906e5fe204e2b7a04365988a7fb6
Becca Refford <beccarefford@gmail.com>
2016-03-01 09:57:35 -0500

    home page fixes

--- wp-content/themes/techgirlz/home.php
+++ wp-content/themes/techgirlz/home.php
@@ -35,91 +35,87 @@
   <div class="col-md-4">
   <center>
   <a href="/techshopz/"><i class="fa fa-cogs fa-5x"></i></a>
-  <h2>What We Do</h2>
-    <p>We empower middle school girls to explore technology through
-      free, hands-on workshops.</p>
+  <h2>Free Workshops</h2>
+    <p>We put all types of tech in the hands of middle school girls through free, project-based workshops. Browse events and register your daughter today.</p>
   </center>
   </div>
 
   <div class="col-md-4">
   <center>
-  <a href="/techshopz-in-a-box/"><i class="fa fa-download fa-5x"></i></a>
-  <h2>How We Do It</h2>
-    <p>By creating tech-centered workshop plans that are free to all.
-    This approach helps us reach more girls in more places.</p>
+  <a href="/techshopz-in-a-box/"><i class="fa fa-users fa-5x"></i></a>
+  <h2>Teach A Workshop</h2>
+    <p>You have the expertise, we have the materials. Use our tried-and true lesson plans to run a TechGirlz workshop in your own town.</p>
   </center>
   </div>
 
   <div class="col-md-4">
   <center>
-  <a href="/blog/"><i class="fa fa-users fa-5x"></i></a>
-  <h2>Why We Do It</h2>
+  <a href="/about/"><i class="fa fa-star fa-5x"></i></a>
+  <h2>About TechGirlz</h2>
     <p>We believe it takes a village to change girls' minds about tech,
-    so we're enabling communities to engage them.</p>
+    so we're enabling communities to engage them. Learn more about our mission.</p>
   </center>
+  </div>
+ </div>
+</div>
+
+<div class="banner">
+  <div class="effect">
+    <center><h1>The TechGirlz Effect</h1></center>
   </div>
 </div>
 
 <div class="spacing">&nbsp;</div>
-<hr />
+
+<div id="home-content">
+  <div class="row">
+    <div class="col-md-7">
+      <center>
+      <img src="/wp-content/uploads/2016/02/Screen-Shot-2016-02-29-at-6.42.50-PM.png">
+      <br />
+      <br />
+      <br />
+      <p>
+        Demand for our workshops is growing, and so are we! Here's how many TechShopz we've
+        run per school year.
+      </p>
+    </center>
+    </div>
+
+    <div class="col-md-3 col-md-offset-1">
+      <center><img src="/wp-content/uploads/2016/02/Screen-Shot-2016-02-29-at-6.09.53-PM.png"></center>
+      <p>
+        <br />
+        of girls were <strong>more interested</strong> in tech after one workshop.
+      </p>
+    </div>
+  </div>
+</div>
+
 <div class="spacing">&nbsp;</div>
 
-<center><h3>After just one of our workshops...</h3></center>
+<div class="banner">
+  <div class="revolution">
+    <center><h1>Be Part of the Revolution of Girls in Tech</h1></center>
 
   <div class="row">
-    <div class="col-md-6">
-      <h1><span>81%</span></h1>
-      <p>
-        of girls were said they were more interested in technology.
-      </p>
-      <span><i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-      </span>
-      <i class="fa fa-female fa-5x"></i>
-      <i class="fa fa-female fa-5x"></i>
-    </div>
+      <div class="col-md-4 nopadding">
+        <img src="/wp-content/uploads/2016/02/Teach.jpg">
+        <h2>Teach <i class="fa fa-arrow-circle-right"></i></h2>
+      </div>
 
-    <div class="col-md-6">
-      <h1><span>70%</span></h1>
-      <p>
-        of girls said they were more likely to consider a career in technology.
-      </p>
-        <span><span>
-        <i class="fa fa-female fa-5x"></i>
-        <i class="fa fa-female fa-5x"></i>
-        <i class="fa fa-female fa-5x"></i>
-        <i class="fa fa-female fa-5x"></i>
-        <i class="fa fa-female fa-5x"></i>
-        <i class="fa fa-female fa-5x"></i>
-        <i class="fa fa-female fa-5x"></i>
-      </span></span>
-        <i class="fa fa-female fa-5x"></i>
-        <i class="fa fa-female fa-5x"></i>
-        <i class="fa fa-female fa-5x"></i>
-    </div>
+      <div class="col-md-4 nopadding">
+        <img src="/wp-content/uploads/2016/02/Volunteer.jpg">
+        <h2>Volunteer <i class="fa fa-arrow-circle-right"></i></h2>
+      </div>
+
+      <div class="col-md-4 nopadding">
+        <img src="/wp-content/uploads/2016/02/Donate.jpg">
+        <h2>Donate <i class="fa fa-arrow-circle-right"></i></h2>
+      </div>
   </div>
 
-<center>
-  <div class="spacing">&nbsp;</div>
-
-  <div class="row">
-    <div class="col-md-12">
-      <a href="/techshopz-in-a-box">
-        <div class="btn">
-          <p>Help Us Reach More</p>
-        </div>
-      </a>
-    </div>
-  </div>
-
-</center>
-
+</div>
 </div>
 
 <?php get_footer(); ?>

4b022c2a79fe4c43f6608c2e9f1435a69ef25284
Becca Refford <beccarefford@gmail.com>
2016-03-07 15:41:20 -0500

    adding code to functions.php to speed up loading time

--- wp-content/themes/techgirlz/functions.php
+++ wp-content/themes/techgirlz/functions.php
@@ -190,3 +190,12 @@ function myButton($atts, $content = ''){
 add_shortcode('mybutton', 'myButton');
 
 require_once('wp_bootstrap_navwalker.php');
+
+function _remove_script_version( $src ){
+	$parts = explode( '?', $src );
+	return $parts[0];
+}
+add_filter( 'script_loader_src', '_remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
+
+?>

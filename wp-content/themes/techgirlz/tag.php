7d21f56df213729896facf8932514afbf5992d43
Becca Refford <beccarefford@gmail.com>
2016-03-12 13:10:27 -0500

    creating tag page template

--- wp-content/themes/techgirlz/tag.php
+++ wp-content/themes/techgirlz/tag.php
@@ -11,7 +11,7 @@ Template Name: Tag Page
 <div class="page-landing" style="background-image:
 url('/wp-content/uploads/2016/03/12906601514_aeb6bcca1a_o.jpg')">
 	<div class="topArea">
-		<h1><?php the_title(); ?></h1>
+		<h1>Blog</h1>
 	</div>
 </div>
 
@@ -22,7 +22,29 @@ url('/wp-content/uploads/2016/03/12906601514_aeb6bcca1a_o.jpg')">
 					<a href="/tag/newsletter">Newsletters</a>
 				</div>
 
-			<div class="archive-blog-content" id="page-content">
+<div class="archive-blog-content" id="page-content">
 
-      </div>
+            <?php
+            while (have_posts()) : the_post();
+            $page_tags = get_post_meta($post->ID, 'tags', true); ?>
+            <div class="row">
+      			<div class="col-md-5">
+      				<div class="blog-image">
+      					<a href="<?php the_permalink(); ?>">
+      					<?php echo get_the_post_thumbnail($post->ID, 'full'); ?>
+      				</a>
+      				</div>
+      			</div>
+
+      			<div class="col-md-7">
+      			<a href="<?php the_permalink(); ?>"><h2><?php echo the_title(); ?></h2></a>
+      			<?php
+      			$content = get_the_content();
+      			$permalink = get_permalink(); ?>
+      			<p><?php echo wpse_custom_excerpts($content, 100, $permalink); ?></p>
+      			<a href="<?php the_permalink(); ?>"><div class="btn">Read More</div></a>
+            <?php endwhile;?>
+</div>
+</div>
+</div><!-- #content -->
 <?php get_footer(); ?>

80b6d0245dc6a1adf3b7a73b1439a587d2574e7c
Becca Refford <beccarefford@gmail.com>
2016-03-12 10:53:16 -0500

    temporarily removing tag links until tag template is styled

---
+++ wp-content/themes/techgirlz/tag.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Template Name: Tag Page
+*/
+
+?>
+
+<?php get_header(); ?>
+
+<div class="page-landing" style="background-image:
+url('/wp-content/uploads/2016/03/12906601514_aeb6bcca1a_o.jpg')">
+	<div class="topArea">
+		<h1><?php the_title(); ?></h1>
+	</div>
+</div>
+
+				<div class="columnBlogDeco">
+					<a href="/tag/press">Press</a> &bull;
+					<a href="/tag/infographic">Infographics</a> &bull;
+					<a href="/tag/video">Videos</a> &bull;
+					<a href="/tag/newsletter">Newsletters</a>
+				</div>
+
+			<div class="archive-blog-content" id="page-content">
+
+      </div>
+<?php get_footer(); ?>

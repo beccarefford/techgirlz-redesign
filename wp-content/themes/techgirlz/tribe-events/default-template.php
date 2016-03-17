a1c1e50efae7dfa5c8a266c04e1ac65a462ba21c
Becca Refford <beccarefford@gmail.com>
2016-03-17 12:45:00 -0400

    fixing Raleigh Durham text at bottom of events page

--- wp-content/themes/techgirlz/tribe-events/default-template.php
+++ wp-content/themes/techgirlz/tribe-events/default-template.php
@@ -46,12 +46,13 @@ url('/wp-content/uploads/2016/03/12906601514_aeb6bcca1a_o.jpg')">
   <div class="col-md-6">
     <h2>Looking for workshops in North Carolina?</h2>
     <p>
-      Our friends in the Raleigh-Durham area have an entire TechGirlz community.
-      <a href="/raleigh-durham-techgirlz-group/">Check out their page</a> for events, mailing lists, and more.
+      <a href="/raleigh-durham-techgirlz-group/">The Raleigh-Durham TechGirlz Group</a> is an entire TechGirlz community in North Carolina.
+      Check out their page for events, or <a href="http://techgirlz.us10.list-manage.com/subscribe?u=abcebd5b4b7aec29f1589129f&id=b2553e542c">sign
+				up for their mailing list</a> to hear about events first.
     </p>
     <center>
       <a href="http://twitter.com/tritechgirlz"><i class="fa fa-twitter fa-2x"></i></a> &nbsp; &nbsp; &nbsp; &nbsp;
-      <a href="http://techgirlz.us10.list-manage.com/subscribe?u=abcebd5b4b7aec29f1589129f&id=b2553e542c"><i class="fa fa-envelope fa-2x"></i></a>
+      <a href="http://www.facebook.com/TriangleTechGirlz"><i class="fa fa-facebook fa-2x"></i></a>
     </center>
   </div>
 

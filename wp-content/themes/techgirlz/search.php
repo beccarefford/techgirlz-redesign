2d6414f14a49bf4539d436cf8a493f2589e86214
Becca Refford <beccarefford@gmail.com>
2016-04-17 14:07:43 -0400

    adding search pbar and search page

--- wp-content/themes/techgirlz/search.php
+++ wp-content/themes/techgirlz/search.php
@@ -9,39 +9,51 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+
+				<div class="page-landing" style="background-image:
+				url('/wp-content/uploads/2016/03/12906601514_aeb6bcca1a_o.jpg')">
+					<div class="topArea">
+						<h1>YOUR SEARCH "<?php the_search_query(); ?>"</h1>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6 nopadding">
+							<div class="columnPageDeco"></div>
+					</div>
+
+					<div class="col-md-6 nopadding">
+						<div class="columnPageDeco2"></div>
+					</div>
+				</div>
+
+	<div class="page-content" id="page-content">
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'techgirlz' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-				?>
+				<div class="row">
+					<div class="col-md-12">
+						<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+						<?php
+						$content = get_the_content();
+						$permalink = get_permalink(); ?>
+						<p><?php echo wpse_custom_excerpts($content, 100, $permalink); ?></p>
+						<div align="right">
+							<a href="<?php the_permalink(); ?>"><div class="btn">Read More</div></a>
+						</div>
+					</div>
+				</div>
 
 			<?php endwhile; ?>
 
 			<?php the_posts_navigation(); ?>
 
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
 		<?php endif; ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+	</div>
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
